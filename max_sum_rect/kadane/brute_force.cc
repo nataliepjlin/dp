04536a62f780202958d3377bc909533c63a14fc2
nat <nataliepjlin@gmail.com>
2024-01-11 16:28:18 +0800

    maxsum adjustment location fixed

---
+++ max_sum_rect/kadane/brute_force.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int n; 
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int maxsum = INT_MIN;
+    for(int i = 0; i < n; i++){
+        int nowsum = 0;
+        for(int j = i; j < n; j++){
+            nowsum += a[j];
+            cout << "nowsum = " << nowsum << endl;
+            maxsum = max(nowsum, maxsum);
+        }
+        cout << endl;
+    }
+    cout << maxsum << endl;
+}
